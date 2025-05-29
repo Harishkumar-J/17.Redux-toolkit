8927a92632ea9c57cd95ae14c7d23715fb5f0b0e
Harishkumar-J <harishwells@gmail.com>
2025-05-29 16:32:29 +0100

    Send data to backend and show notification

--- src/App.js
+++ src/App.js
@@ -23,37 +23,140 @@
 
 //1---------------------------------------------------------------------
 
-//2 USING USEEFFECT WITH REDUX
+// //2 USING USEEFFECT WITH REDUX
 
-//2.1
+// //2.1
+// import { useEffect } from "react";
+// import Cart from "./components/Cart/Cart";
+// import Layout from "./components/Layout/Layout";
+// import Products from "./components/Shop/Products";
+// import { useSelector } from "react-redux";
+
+// function App() {
+//   //2.1 working out the cart data
+//   const cart = useSelector((state) => state.cart);
+//   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+//   //2.2 http call to update the state changes in the backend storage(Firebase realtime database)
+//   // using PUT method to overwrite the changes
+//   useEffect(() => {
+//     fetch(
+//       "https://shop-cart-redux-3f2e8-default-rtdb.firebaseio.com/cart.json",
+//       {
+//         method: "PUT",
+//         body: JSON.stringify(cart),
+//       }
+//     )
+//   }, [cart]);
+
+//   return (
+//     <Layout>
+//       {showCart && <Cart />}
+//       <Products />
+//     </Layout>
+//   );
+// }
+
+// export default App;
+
+//2-----------------------------------------------------------------------------
+
+//3 Showing Notification for cart item changes
+// to notify if the item added in backend we need async await to capture thre response
+// or to deal with the side effects
+
 import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+//3.3 to use the notification reducer import useDispatch and actions
+import { useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+//3.10
+import Notification from "./components/UI/Notification";
+import { Fragment } from "react";
+
+//3.10 to restrict the sendCartData in useEffect to run at 2nd time
+// we use a global variable
+
+let isInitial = true;
 
 function App() {
-  //2.1 working out the cart data
   const cart = useSelector((state) => state.cart);
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  //3.8 displaying the notification
+  const notification = useSelector((state) => state.ui.notification);
+  //3.3
+  const dispatch = useDispatch();
+  //3.4 null
 
-  //2.2 http call to update the state changes in the backend storage(Firebase realtime database)
-  // using PUT method to overwrite the changes
   useEffect(() => {
-    fetch(
-      "https://shop-cart-redux-3f2e8-default-rtdb.firebaseio.com/cart.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
+    //3.1 async-await
+    const sendCartData = async () => {
+      //3.5 keep the notification as pending before we get a response
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
+      const response = await fetch(
+        "https://shop-cart-redux-3f2e8-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      //3.2 error if not data sent
+      if (!response.ok) {
+        throw new Error("Failed to send Cart data");
       }
-    );
-  }, [cart]);
+
+      //3.5 if response is ok
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Item added to the cart!",
+        })
+      );
+    };
+
+    //3.11 does not block in the second time
+    if (isInitial === true) {
+      isInitial = false;
+      return;
+    }
+    //3.6 execute the sendCartData function and also catch the error
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+    //3.7 dispatch in dependency
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    //3.9 Using the Notification component
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,18 +1,45 @@
-// this containds the state,reducer func and actions of cart components
-//1.1
+// // this containds the state,reducer func and actions of cart components
+// //1.1
+// import { createSlice } from "@reduxjs/toolkit";
+
+// //1.2 creating the ui Slice
+// const uiSlice = createSlice({
+//   name: "ui",
+//   initialState: { cartIsVisible: false },
+//   reducers: {
+//     toggle(state) {
+//       state.cartIsVisible = !state.cartIsVisible;
+//     },
+//   },
+// });
+// //1.4 also export actions which contains all the functions in reducer
+// export const uiActions = uiSlice.actions
+// //1.3 export the slice
+// export default uiSlice;
+
+//1---------------------------------------------
+
+//2.1 notification for sending cart data status
 import { createSlice } from "@reduxjs/toolkit";
 
-//1.2 creating the ui Slice
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false },
+  //2.2 adding notification state
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
     },
+    //2.3 reducer function for notification
+    showNotification(state, action) {
+      //2.4 setting then state to have status value received from action payload
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
-//1.4 also export actions which contains all the functions in reducer
-export const uiActions = uiSlice.actions
-//1.3 export the slice
+export const uiActions = uiSlice.actions;
 export default uiSlice;
